49e5873e7ef0ca61f78063e326823e03677aadd0
Adam-Hammo <43816242+Adam-Hammo@users.noreply.github.com>
2020-09-17 10:14:12 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='arlulaapi',  
-     version='1.1.8',
+     version='1.1.9',
      author="Adam Hamilton",
      author_email="adamhammo99@gmail.com",
      description="A package to facilitate access to the Arlula Imagery Marketplace API",
      
      long_description=long_description,
    long_description_content_type="text/markdown",
-     url="https://github.com/Adam-Hammo/arlulaapi.git",
+     url="https://github.com/Arlula/python-archive-sdk.git",
      packages=["arlulaapi"],
     install_requires=['grequests', 'requests', 'pgeocode'],
      classifiers=[
