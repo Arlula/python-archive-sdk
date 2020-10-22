9ac573012fe6a48acd5d809be622048a1cbf10c9
Adam-Hammo <43816242+Adam-Hammo@users.noreply.github.com>
2020-10-22 11:46:54 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='arlulaapi',
-<<<<<<< HEAD
-    version='1.2.1',
-=======
     version='1.2.2',
->>>>>>> uat
     author="Adam Hamilton",
     author_email="adamhammo99@gmail.com",
     description="A package to facilitate access to the Arlula Imagery Marketplace API",
